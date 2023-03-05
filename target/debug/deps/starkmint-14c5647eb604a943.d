ab4a05d96888ea636085859e54f4d5abb7e00920
Kari Evergreen <evergreenkary@gmail.com>
2023-03-06 02:05:38 +0800

    🎉 initial commit

---
+++ target/debug/deps/starkmint-14c5647eb604a943.d
@@ -0,0 +1,5 @@
+/home/kari/project/local/starkmint/target/debug/deps/starkmint-14c5647eb604a943.rmeta: starkmint/src/main.rs
+
+/home/kari/project/local/starkmint/target/debug/deps/starkmint-14c5647eb604a943.d: starkmint/src/main.rs
+
+starkmint/src/main.rs:
